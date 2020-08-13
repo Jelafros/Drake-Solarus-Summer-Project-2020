e7d5e0bdb79087a0e373776c138cb61dc5fcdce2
katjaMathesius <67726169+katjaMathesius@users.noreply.github.com>
2020-08-13 16:21:09 -0500

    Updated first map

--- maps/first_map.lua
+++ maps/first_map.lua
@@ -7,18 +7,33 @@
 -- See the Solarus Lua API documentation:
 -- http://www.solarus-games.org/doc/latest
 
+
 local map = ...
 local game = map:get_game()
+
 
 -- Event called at initialization time, as soon as this map is loaded.
 function map:on_started()
 
   -- You can initialize the movement and sprites of various
   -- map entities here.
+
+  
 end
 
 -- Event called after the opening transition effect of the map,
 -- that is, when the player takes control of the hero.
 function map:on_opening_transition_finished()
+
+end
+
+function Grandma:on_interaction()
+
+  if game:get_value("questComplete")
+  then
+    game:start_dialog("grandma.questOver")
+  else
+    game:start_dialog("grandma.questStart")
+  end
 
 end
